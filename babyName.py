4ffa43427b30ae5dfc52d10ec53fde75eb34f72b
marmik pandya <marmikpandya@gmail.com>
2016-02-11 00:17:08 -0500

    Done now doing the documentations

--- babyName.py
+++ babyName.py
@@ -2,24 +2,6 @@ import glob
 import pandas as pd
 import numpy as np
 
-
-# Formatted output strings
-popularity_outputMale = "The most popular male name is %s, with a total count of %i"
-popularity_outputFemale = "The most popular female name is %s, with a total count of %i"
-ambiguous_name_output = "The most gender ambiguous name in %s is %s: %d girls and %d boys."
-winners_losers_output = (
-    "From %s to %s the name with the greatest increase " 
-    "in popularity was %s, with a %2.1f%% increase.\n" 
-    "The name with the greatest decline in popularity was "
-    "%s, with a %2.1f%% change."
-)
-extrapolated_output = (
-    "Inferring from truncated values, from %s to %s the name "
-    "with the greatest growth was %s,\nand the name with "
-    "the greatest decline was %s."
-)
-
-
 # Returns True if SEX column contains both M and F, False otherwise
 def has_both_sexes(input_frame):
     male = np.any(input_frame.SEX.str.contains("M", case=False))
@@ -51,10 +33,7 @@ def most_popular_name(names_df,gender):
     return tuple(dfgroups.values[0]) 
   
 
-# Find most gender ambiguous name in 2013 and 1945
 # Ambiguity is calculated by difference in male and female totals, lower is more ambiguous
-
-# Returns a tuple of (year, name, female_count, male_count)
 def ambiguous_name_by_year(year, names_df):
     ARGYEAR = str(year)
     
@@ -95,7 +74,7 @@ def winners_and_losers(year, names_df, extrapolate=False):
     # Names that don't exist in one of the years are propagated as NaN and not considered,
     # unless extrapolate=True
     
-    # We can extrapolate some possible results from truncated data by assuming the most extreme case.
+    # Extrapolation gives possible winner and loser
     if extrapolate:
         names_current.fillna(value=1, axis='columns', inplace=True)
         
@@ -112,7 +91,7 @@ def winners_and_losers(year, names_df, extrapolate=False):
     biggest_loser_name = biggest_loser.index[0]
     biggest_loser_pct = biggest_loser.values[0] * 100
     
-    # If we're extrapolating from truncated data, percentage changes aren't accurate.
+    # If we're extrapolating the percentage changes aren't accurate.
     if extrapolate:
         return START_YEAR, LAST_YEAR, biggest_winner_name, biggest_loser_name
     else:
@@ -121,15 +100,13 @@ def winners_and_losers(year, names_df, extrapolate=False):
 
 
 def main():
-    print("Reading in data... ", end="", flush=True)
     state_names = get_data()
-    print("Done.")
-    print(popularity_outputFemale % most_popular_name(state_names,"F"))
-    print(popularity_outputMale % most_popular_name(state_names,"M"))
-    print(ambiguous_name_output % ambiguous_name_by_year(2013, state_names))
-    print(ambiguous_name_output % ambiguous_name_by_year(1945, state_names))
-    print(winners_losers_output % winners_and_losers(1980, state_names))
-    print(extrapolated_output % winners_and_losers(1980, state_names, extrapolate=True))
+    print("The most popular female name is %s, with a total count of %i" % most_popular_name(state_names,"F"))
+    print("The most popular male name is %s, with a total count of %i" % most_popular_name(state_names,"M"))
+    print("The most gender ambiguous name in %s is %s: %d girls and %d boys." % ambiguous_name_by_year(2013, state_names))
+    print("The most gender ambiguous name in %s is %s: %d girls and %d boys." % ambiguous_name_by_year(1945, state_names))
+    print("From %s to %s the name with the greatest increase in popularity was %s, with a %2.1f%% increase.\n The name with the greatest decline in popularity was %s, with a %2.1f%% change." % winners_and_losers(1980, state_names))
+    print( "Inferring from extrapolated values, from %s to %s the name with the greatest growth was %s,\nand the name with the greatest decline was %s." % winners_and_losers(1980, state_names, extrapolate=True))
     
 if __name__ == "__main__":
     main()
--- modelling.py
+++ modelling.py
@@ -1,3 +1,7 @@
+# Since this was my first experience with data science and data modelling as such 
+# refered to a lot of StackOverflow examples 
+# a few tutorials on the web
+
 import csv
 import pandas as pd
 import numpy as np
@@ -5,33 +9,8 @@ from sklearn.preprocessing import LabelEncoder
 import random
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import (accuracy_score,
-                             average_precision_score,
-                             auc,
-                             classification_report,
-                             confusion_matrix,
-                             explained_variance_score,
-                             f1_score,
-                             fbeta_score,
-                             hamming_loss,
-                             hinge_loss,
-                             jaccard_similarity_score,
-                             log_loss,
-                             matthews_corrcoef,
-                             mean_squared_error,
-                             mean_absolute_error,
-                             precision_recall_curve,
-                             precision_recall_fscore_support,
-                             precision_score,
-                             recall_score,
-                             r2_score,
-                             roc_auc_score,
-                             roc_curve,
-                             zero_one_loss)
-
-
-
-
+from sklearn.metrics import mean_squared_error
+                             
 # convert to CSV
 
 txt_file_train = r"codetest_train.txt"
@@ -91,7 +70,7 @@ fullData[num_cols] = fullData[num_cols].fillna(fullData[num_cols].mean(),inplace
 #Impute categorical missing values with -9999
 fullData[cat_cols] = fullData[cat_cols].fillna(value = -9999)
 
-print("After Entering values")
+#print("After Entering values")
 print(fullData.describe())
 
 #create label encoders for categorical features
@@ -100,10 +79,6 @@ for var in cat_cols:
  fullData[var] = number.fit_transform(fullData[var].astype('str'))
 
 #Target variable is also a categorical so convert it
-#fullData["target"] = number.fit_transform(fullData["target"].astype('float'))
-
-print("___________________________Full Data__________________________")
-print(fullData['target'])
 
 Train=fullData[fullData['Type']=='Train']
 test=fullData[fullData['Type']=='Test']
@@ -111,46 +86,28 @@ test=fullData[fullData['Type']=='Test']
 
 #pass the imputed dummy variable into modelling process
 
-print("pass the imputed dummy variable into modelling process")
+#print("pass the imputed dummy variable into modelling process")
 
 features=list(set(list(fullData.columns))-set(target_col)-set(other_col))
 
-print("xtrain and shit")
-
 x_train = Train[list(features)].values
-
-print("####################################X_train##################################")
-
-print(x_train)
-
+#print("####################################X_train##################################")
+#print(x_train)
 y_train = Train["target"].values
 
-print("####################################y_train##################################")
-
-print(y_train)
-
+#print("####################################y_train##################################")
+#print(y_train)
 x_test=test[list(features)].values
 
-print("starting random seeeeeed")
+#print("starting random seeeeeed")
 
 random.seed(100)
-
-print("Seeding Done")
-
 rf = RandomForestClassifier(n_estimators=10, n_jobs = -1)
-
-print("Random Forest Function Call done")
-
-
 rf.fit(x_train, y_train.astype('str'))
-
-print("Random Seeding fitting done")
-
 final_status = rf.predict(x_test)
-
-print(final_status)
 test["target"]=final_status
-print("Final STATUS")
+
+#print("Final STATUS", final_status)
 test.to_csv('model_output.csv',columns=['target'])
 
 
